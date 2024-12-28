5dade7edccf465dd067e54c5ee60c49e0bb356c5
Iorie Chua <chuaioriecollege@gmail.com>
2024-12-28 20:45:48 +0800

    implement transaction card, local storage and dynamic rendering

--- js/transactions.js
+++ js/transactions.js
@@ -1,17 +1,51 @@
-const transactions = [];
+let transactions = [];
+
+const createCard = (transaction) => {
+  let card = document.createElement("div");
+  card.classList.add(`${transaction.type}-card`);
+  card.innerHTML = `
+  <div class="card-header">
+    <span class="amount">
+      ${transaction.type === "income" ? "+" : "-"}₱${Math.abs(
+    transaction.amount
+  )}
+    </span>
+    <span class="time-date">
+      ${transaction.date} | ${transaction.time}
+    </span>
+  </div>
+  <div class="card-details">
+    <p><strong>Category:</strong> ${transaction.category}</p>
+    <p><strong>Note:</strong> ${transaction.note}</p>
+  </div>
+  `;
+
+  return card;
+};
+
+transactions = JSON.parse(localStorage.getItem("transactions"))
+  ? JSON.parse(localStorage.getItem("transactions"))
+  : [];
+
+const transactionCards =
+  document.getElementsByClassName("transaction-cards")[0];
+for (let i = 0; i < transactions.length; i++) {
+  transactionCards.appendChild(createCard(transactions[i]));
+}
 
 document.getElementsByTagName("form")[0].onsubmit = (e) => {
   e.preventDefault();
   const form = e.target;
   const [h, m] = form.time.value.split(":");
   const [year, month, day] = form.date.value.split("-");
-  const dateObj = new Date(year, month - 1, day);
+  const dateObj = new Date(year, month - 1, day, h, m);
   const [dayStr, monthStr, dayOfMonth, yearStr] = dateObj
     .toDateString()
     .split(" ");
 
   const transaction = {
     type: form.income.checked === true ? "income" : "expense",
+    dateObj,
     date: `${monthStr} ${dayOfMonth}, ${yearStr}`,
     time: `${h % 12 ? h % 12 : 12}:${m} ${h >= 12 ? "PM" : "AM"}`,
     amount:
@@ -21,5 +55,32 @@ document.getElementsByTagName("form")[0].onsubmit = (e) => {
     note: form.note.value,
   };
 
-  console.log(transaction);
+  let card = createCard(transaction);
+
+  let i = 0;
+  if (transactions.length === 0) {
+    transactions.push(transaction);
+
+    document.getElementsByClassName("transaction-cards")[0].appendChild(card);
+  } else {
+    for (; i < transactions.length; i++) {
+      if (
+        Number(transactions[i].dateObj) < Number(transaction.dateObj) ||
+        i === transactions.length - 1
+      ) {
+        transactions = [
+          ...transactions.slice(0, i),
+          transaction,
+          ...transactions.slice(i),
+        ];
+        break;
+      }
+    }
+  }
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+
+  let next =
+    document.getElementsByClassName("transaction-cards")[0].children[i];
+  next.parentNode.insertBefore(card, next);
 };
