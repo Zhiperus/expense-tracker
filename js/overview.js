78769b0977c27927e699ef854e7f1ea3c8e1d2f7
Iorie Chua <chuaioriecollege@gmail.com>
2024-12-31 13:51:47 +0800

    working transaction and overview display

--- js/overview.js
+++ js/overview.js
@@ -3,6 +3,7 @@ import Cookies from "./utilities/cookies.js";
 let transactions = [];
 let income = 0;
 let expense = 0;
+const transactionList = document.getElementsByClassName("transaction-list")[0];
 
 transactions = JSON.parse(Cookies.getCookie("transactions"))
   ? JSON.parse(Cookies.getCookie("transactions"))
@@ -12,6 +13,16 @@ transactions.forEach((transaction) => {
   transaction.type === "income"
     ? (income += transaction.amount)
     : (expense += transaction.amount);
+
+  const card = document.createElement("div");
+  card.classList.add("transaction");
+  card.innerHTML = `<span>${transaction.category}</span><div><span class='${
+    transaction.type
+  }'> ${transaction.type === "income" ? "+" : "-"}₱${Math.abs(
+    transaction.amount
+  )}</span><span>${transaction.date}</span></div>`;
+
+  transactionList.appendChild(card);
 });
 
 document.getElementsByClassName("amount-field")[0].children[0].innerHTML +=
